cda5247a359863905bde982974d32d18e5dd33db
jingstromer <jingstromer@gmail.com>
2015-05-07 17:24:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+(function(){
 var app = angular.module('jirApp', []);
+
+app.controller('JirController', function() {
+  this.product = degreeCalculator;
+});
+
+var degreeCalculator = {
+  fahrenheit: 3.24,
+}
+
+})();
