79272c75ed5fde201f7ea5aad19a008dbaa02625
jingstromer <jingstromer@gmail.com>
2015-05-08 13:26:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,18 @@
 
   var fcCalculator = {
     celsius: 0,
-    fahrenheit: 0
-    /*
+    fahrenheit: 0,
+  
     fCalc: function () {
       fahrenheit = 32 + (9/5) * celsius;
       return fahrenheit;
-    }
+    },
+    
      cCalc: function () {
       cesius = (fahrenheit - 32) * (5/9);
       return celsius;
     }
-    */
+  
   }
 
 })();
