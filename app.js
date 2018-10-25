8b1731f432849c832f7081d6c15978f813eea4f6
jingstromer <jingstromer@gmail.com>
2018-10-25 16:13:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,15 @@
     },
     
      cCalc: function () {
-      this.celsius = (this.fahrenheit - 32) * (5/9);
+      //this.celsius = (this.fahrenheit - 32) * (5/9);
+      //this.celsius = curl https://api.keyvalue.xyz/a17766c6/myKey;
+      const Http = new XMLHttpRequest();
+      const url='https://jsonplaceholder.typicode.com/posts';
+      Http.open("GET", url);
+      Http.send();
+      Http.onreadystatechange=(e)=>{
+          this.celsius = Http.responseText;
+      }
     }
   
   }
